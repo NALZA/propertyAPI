19440eefa8300c86b1255c8e1a8a174ebe86c3f9
M Nally <mitchellnally@msn.com>
2021-07-19 11:34:20 +1000

    fixed broken test for retrieving all properties

--- routes/properties.js
+++ routes/properties.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+//storage for properties
+const properties = [
+	{
+		id: 1,
+		address: {
+			addressLine: '51 Tree Street',
+			suburb: 'Brunswick',
+			city: 'Melbourne',
+			postcode: '1234',
+		},
+		description: '52 Tree Street is a nice home',
+		price: 560000.0,
+	},
+];
+
 /* GET property listing. */
 router.get('/', function (req, res, next) {
-	res.send('respond with a resource');
+	res.json(properties);
 });
 
 module.exports = router;
