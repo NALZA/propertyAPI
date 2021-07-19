4a31b3541d503dd4a287f171d3a64cbb197edf62
M Nally <mitchellnally@msn.com>
2021-07-19 11:52:19 +1000

    added route to fix pass test to add property

--- routes/properties.js
+++ routes/properties.js
@@ -21,4 +21,26 @@ router.get('/', function (req, res, next) {
 	res.json(properties);
 });
 
+/* Post property listing. */
+router.post('/', function (req, res, next) {
+	const { body } = req;
+
+	const newProperty = {
+		//no facility to delete this will work for autoincrement
+		id: properties.length + 1,
+		address: {
+			addressLine: body.address.addressLine,
+			suburb: body.address.suburb,
+			city: body.address.city,
+			postcode: body.address.postcode,
+		},
+		description: body.description,
+		price: body.price,
+	};
+
+	properties.push(newProperty);
+
+	res.status(201).json(newProperty);
+});
+
 module.exports = router;
