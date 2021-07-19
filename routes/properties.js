1afb8d9cc948c5f17a91fc0e01e9587a9548abe5
M Nally <mitchellnally@msn.com>
2021-07-19 14:05:12 +1000

    added ablitlty to filter by suburb and append an average tag

--- routes/properties.js
+++ routes/properties.js
@@ -14,11 +14,52 @@ const properties = [
 		description: '52 Tree Street is a nice home',
 		price: 560000.0,
 	},
+	{
+		id: 2,
+		address: {
+			addressLine: '111 Bush Street',
+			suburb: 'MoonePonds',
+			city: 'Melbourne',
+			postcode: '1224',
+		},
+		description: '111 Bush Street is not in Brunswick',
+		price: 560000.0,
+	},
+	{
+		id: 3,
+		address: {
+			addressLine: '5 apple Road',
+			suburb: 'Brunswick',
+			city: 'Melbourne',
+			postcode: '1234',
+		},
+		description: '5 apple Road',
+		price: 520000.0,
+	},
 ];
 
 /* GET property listing. */
 router.get('/', function (req, res, next) {
 	res.json(properties);
+});
+
+/* GET property listing by suburb */
+router.get('/:suburb', function (req, res, next) {
+	const foundProperties = properties.filter(
+		(property) => property.address.suburb === req.params.suburb
+	);
+
+	const average =
+		foundProperties.reduce((r, v) => r + v.price, 0) /
+		foundProperties.length;
+
+	for (const prop of foundProperties) {
+		if (prop.price == average) prop.average = 'Equal';
+		else if (prop.price < average) prop.average = 'Less';
+		else if (prop.price > average) prop.average = 'Greater';
+	}
+	console.log(foundProperties);
+	res.json(foundProperties);
 });
 
 /* Post property listing. */
